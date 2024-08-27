233a8eba6bae0887a23ac70d17834eda15b3d34e
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-08-27 23:09:07 +0500

    Working on Google map for locations

--- src/components/Map/Map.jsx
+++ src/components/Map/Map.jsx
@@ -1,10 +1,23 @@
 import React from 'react'
-
+import googleMapReact from 'google-map-react'
+import useStyles from './style'
+import { useMediaQuery } from '@mui/material'
 const Map = () => {
+  const classes = useStyles()
+  const isMobile = useMediaQuery('(min-width:600px)')
+  const coordinates = { lat:0,lng:0}
   return (
-    <div>
-      <h1>Google map is here</h1>
-    </div>
+    <>
+      <googleMapReact
+        bootstrapURLKeys={{ key: process.env.REACT_APP_GOOGLE_MAP_API_KEY }}
+        defaultCenter={coords}
+        center={coords}
+        defaultZoom={14}
+        margin={[50, 50, 50, 50]}
+      >
+
+      </googleMapReact>
+    </>
   )
 }
 

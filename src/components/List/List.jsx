3fc1dceac379b87e90253f6e15a280e547e7004a
MuhammadSarfrazAfzal <sarfrazafzal790@gmail.com>
2024-08-28 22:41:08 +0500

    Setting up list component for Sorting of Restaurants

--- src/components/List/List.jsx
+++ src/components/List/List.jsx
@@ -1,9 +1,31 @@
 import React from 'react'
-
+import { CircularProgress,Grid,Typography,InputLabel,MenuItem,FormControl,Select } from '@mui/material'
+import useStyles from './style'
+import { useState } from 'react'
 const List = () => {
+  const classes = useStyles()
+  const [type,setType]=useState('Restaurants')
+  const [ratings,setRating]=useState('')
   return (
-    <div>
-      <h1>Here is list</h1>
+    <div className={classes.container}>
+      <Typography variant='h5'>Resturants,Hotels and Attractions arround you</Typography>
+      <FormControl className={classes.formControl}>
+            <InputLabel id="type">Type</InputLabel>
+            <Select id="type" value={type} onChange={(e) => setType(e.target.value)}>
+              <MenuItem value="restaurants">Restaurants</MenuItem>
+              <MenuItem value="hotels">Hotels</MenuItem>
+              <MenuItem value="attractions">Attractions</MenuItem>
+            </Select>
+          </FormControl>
+          <FormControl className={classes.formControl}>
+            <InputLabel id="rating">Rating</InputLabel>
+            <Select id="rating" value={rating} onChange={(e) => setRating(e.target.value)}>
+              <MenuItem value="">All</MenuItem>
+              <MenuItem value="3">Above 3.0</MenuItem>
+              <MenuItem value="4">Above 4.0</MenuItem>
+              <MenuItem value="4.5">Above 4.5</MenuItem>
+            </Select>
+          </FormControl>
     </div>
   )
 }
--- src/components/List/style.js
+++ src/components/List/style.js
@@ -2,6 +2,23 @@ import { makeStyles } from '@mui/styles';
 import { createTheme, ThemeProvider } from '@mui/material/styles';
 const theme = createTheme();
 const useStyles = makeStyles((theme) => ({
-    
+  formControl: {
+    margin: "2px", minWidth: 120, marginBottom: '30px',
+  },
+  selectEmpty: {
+    marginTop: "1px",
+  },
+  loading: {
+    height: '600px', display: 'flex', justifyContent: 'center', alignItems: 'center',
+  },
+  container: {
+    padding: '10px',
+  },
+  marginBottom: {
+    marginBottom: '30px',
+  },
+  list: {
+    height: '75vh', overflow: 'auto',
+  }
   }));
 export default useStyles
